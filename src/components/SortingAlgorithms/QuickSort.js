2850ed565018472c48584ef375dca22f218ab0b9
nkuek <nkuek1@gmail.com>
2021-05-10 17:58:01 -0400

    comments to quick sort

--- src/components/SortingAlgorithms/QuickSort.js
+++ src/components/SortingAlgorithms/QuickSort.js
@@ -13,21 +13,26 @@ const quickSortHelper = (array, start, end, animations) => {
     let right = end;
 
     while (left <= right) {
+        // push first time to change color to the selected array color
         animations.push([left, right]);
 
         if (array[left] > array[pivot] && array[right] < array[pivot]) {
+            // push in the index and the new height of that index (essentially animating the swap)
             animations.push([
                 [left, array[right]],
                 [right, array[left]],
             ]);
+            // second push will change the color back to its original color
             animations.push([left, right]);
 
             swap(array, left, right);
         } else {
+            // push in the index and the new height of that index (essentially animating the swap)
             animations.push([
                 [left, array[left]],
                 [right, array[right]],
             ]);
+            // second push will change the color back to its original color
             animations.push([left, right]);
         }
         if (array[left] <= array[pivot]) {
@@ -36,11 +41,14 @@ const quickSortHelper = (array, start, end, animations) => {
         if (array[right] >= array[pivot]) right--;
     }
 
+    // highlight the pivot index and the index of the array to be swapped
     animations.push([pivot, right]);
+    // swap animation [index, new height of index]
     animations.push([
         [right, array[pivot]],
         [pivot, array[right]],
     ]);
+    // change color back to original color
     animations.push([pivot, right]);
 
     swap(array, pivot, right);
